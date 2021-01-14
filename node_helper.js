5027ec9fedad0e66a9f84ba829d31f992f7313b9
Omar Adobati <omar.adobati@gmail.com>
2021-01-14 17:13:38 +0100

    WIP

--- node_helper.js
+++ node_helper.js
@@ -12,33 +12,47 @@ var request = require('request');
 const Log = require("../../js/logger.js");
 
 module.exports = NodeHelper.create({
+  refreshTime: 2 * 60 * 1000,
+  timeoutScore: [],
+  timeoutTable: [],
+  requestOptions: {
+    method: 'POST',
+    headers: {
+      'Host': 'ta4-data.de',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Connection': 'keep-alive',
+      'Accept': '*/*',
+      'User-Agent': 'TorAlarm/20161202 CFNetwork/808.1.4 Darwin/16.1.0',
+      'Accept-Language': 'de-de',
+      'Accept-Encoding': 'gzip',
+      'Content-Length': '49',
+    },
+    body: '{"lng":"de-DE","device_type":0,"decode":"decode"}',
+    form: false
+  },
+
   start: function () {
-    Log.log(this.name, 'helper started...')
+    Log.log('Starting node helper for:', this.name)
   },
 
   stop: function () {
-    Log.log(this.name, 'helper started...')
+    Log.log('Stopping node helper for:', this.name)
+    ([...this.timeoutScore, ...this.timeoutTable]).forEach(id => clearTimeout(id))
   },
 
   getLeagueIds: function (leagues, showTables) {
-    Log.info(this.name, 'getLeagueIds', 'https://www.ta4-data.de/ta/data/competitions')
+    leagues.forEach(id => {
+      clearTimeout(this.timeoutScore[id]);
+      clearTimeout(this.timeoutTable[id]);
+    })
+    
+    const url = 'https://www.ta4-data.de/ta/data/competitions'
+    Log.debug(this.name, 'getLeagueIds', url)
     var self = this;
     var options = {
-      method: 'POST',
-      url: 'https://www.ta4-data.de/ta/data/competitions',
-      headers: {
-        'Host': 'ta4-data.de',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Connection': 'keep-alive',
-        'Accept': '*/*',
-        'User-Agent': 'TorAlarm/20161202 CFNetwork/808.1.4 Darwin/16.1.0',
-        'Accept-Language': 'de-de',
-        'Accept-Encoding': 'gzip',
-        'Content-Length': '49',
-      },
-      body: '{"lng":"de-DE","device_type":0,"decode":"decode"}',
-      form: false
-    };
+      ...this.requestOptions,
+      url
+    }
 
     request(options, function (error, response, body) {
       if (!error && body) {
@@ -51,8 +65,8 @@ module.exports = NodeHelper.create({
               if (competitions[j].id == leagues[i]) {
                 if (showTables && competitions[j].has_table) {
                   self.getTable(competitions[j].id);
-                } 
-              
+                }
+
                 leagueIds.push(competitions[j].id)
                 self.sendSocketNotification('LEAGUES', {
                   name: competitions[j].name,
@@ -69,121 +83,54 @@ module.exports = NodeHelper.create({
     });
   },
 
-  getTeams: function (leagueId) {
-    Log.info(this.name, 'getTeams', 'https://www.ta4-data.de/ta/data/competitions/' + leagueId.toString() + '/table')
-    var self = this;
-    var options = {
-      method: 'POST',
-      url: 'https://www.ta4-data.de/ta/data/competitions/' + leagueId.toString() + '/table',
-      headers: {
-        'Host': 'ta4-data.de',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Connection': 'keep-alive',
-        'Accept': '*/*',
-        'User-Agent': 'TorAlarm/20161202 CFNetwork/808.1.4 Darwin/16.1.0',
-        'Accept-Language': 'de-de',
-        'Accept-Encoding': 'gzip',
-        'Content-Length': '49',
-      },
-      body: '{"lng":"de-DE","device_type":0,"decode":"decode"}',
-      form: false
-    };
-    request(options, function (error, response, body) {
-      var teamIds = [];
-      var data = JSON.parse(body);
-      for (var i = 0; i < data.data.length; i++) {
-        if (data.data[i].type !== 'table') { continue; }
-        for (var j = 0; j < data.data[i].table.length; j++) {
-          teamIds.push(data.data[i].table[j].team_id);
-        }
-      }
-    });
-  },
-
   getTable: function (leagueId) {
-    Log.info(this.name, 'getTable', 'https://www.ta4-data.de/ta/data/competitions/' + leagueId.toString() + '/table')
+    const url = 'https://www.ta4-data.de/ta/data/competitions/' + leagueId.toString() + '/table'
+    Log.info(this.name, 'getTable', url)
     var self = this;
     var options = {
-      method: 'POST',
-      url: 'https://www.ta4-data.de/ta/data/competitions/' + leagueId.toString() + '/table',
-      headers: {
-        'Host': 'ta4-data.de',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Connection': 'keep-alive',
-        'Accept': '*/*',
-        'User-Agent': 'TorAlarm/20161202 CFNetwork/808.1.4 Darwin/16.1.0',
-        'Accept-Language': 'de-de',
-        'Accept-Encoding': 'gzip',
-        'Content-Length': '49',
-      },
-      body: '{"lng":"de-DE","device_type":0,"decode":"decode"}',
-      form: false
-    };
+      ...this.requestOptions,
+      url
+    }
     request(options, function (error, response, body) {
       if (!error && body) {
         var data = JSON.parse(body);
         data = data.data;
-        for (var i = 0; i < data.length; i++) {
-          if (data[i].type === 'table') {
-            self.sendSocketNotification('TABLE', {
-              leagueId: leagueId,
-              table: data[i].table
-            });
-            Log.debug(self.name, 'getTable | data', JSON.stringify(data, null, 2))
-            var refreshTime = ((data.refresh_time  || (5 * 60)) * 1000);
-            Log.info(self.name, 'getTable | refresh_time', data.refresh_time, refreshTime)
-            setTimeout(function () {
-              self.getTable(leagueId);
-            }, refreshTime);
-            return;
-          }
-        }
-      } else {
-        setTimeout(function () {
-          self.getTable(leagueId);
-        }, 60 * 1000);
+        const tables = data.filter(d => d.type === 'table')
+        self.sendSocketNotification('TABLE', {
+          leagueId: leagueId,
+          table: tables
+        });
       }
     });
   },
 
   getScores: function (leagueId) {
-    Log.info(this.name, 'getScores', 'https://www.ta4-data.de/ta/data/competitions/' + leagueId.toString() + '/matches/round/0')
+    const url = 'https://www.ta4-data.de/ta/data/competitions/' + leagueId.toString() + '/matches/round/0'
+    Log.info(this.name, 'getScores', url)
     var self = this;
     var options = {
-      method: 'POST',
-      url: 'https://www.ta4-data.de/ta/data/competitions/' + leagueId.toString() + '/matches/round/0',
-      headers: {
-        'Host': 'ta4-data.de',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Connection': 'keep-alive',
-        'Accept': '*/*',
-        'User-Agent': 'TorAlarm/20161202 CFNetwork/808.1.4 Darwin/16.1.0',
-        'Accept-Language': 'de-de',
-        'Accept-Encoding': 'gzip',
-        'Content-Length': '49',
-      },
-      body: '{"lng":"de-DE","device_type":0,"decode":"decode"}',
-      form: false
+      ...this.requestOptions,
+      url
     }
 
     request(options, function (error, response, body) {
       if(!error && body) {
         var data = JSON.parse(body);
         Log.debug(self.name, 'getScores | data', JSON.stringify(data, null, 2))
-        var refreshTime = ((data.refresh_time  || (5 * 60)) * 1000);
-        Log.info(self.name, 'getScores | refresh_time', data.refresh_time, refreshTime)
+        self.refreshTime = ((data.refresh_time  || (5 * 60)) * 1000);
+        Log.debug(self.name, 'getScores | refresh_time', data.refresh_time, self.refreshTime)
         var standings = data.data;
         self.sendSocketNotification('STANDINGS', {
           leagueId: leagueId,
           standings: standings
         });
-        setTimeout(function () {
+        self.timeoutScore[leagueId] = setTimeout(function () {
           self.getScores(leagueId);
-        }, refreshTime);
+        }, self.refreshTime);
       } else {
-        setTimeout(function () {
-          self.getTable(leagueId);
-        }, 60 * 1000);
+        self.timeoutScore[leagueId] = setTimeout(function () {
+          self.getScores(leagueId);
+        }, 5 * 60 * 1000);
       }
     });
   },
