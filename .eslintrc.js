e61128181ff80a348f85d6eb601067a98e1e7932
Omar Adobati <omar.adobati@gmail.com>
2021-04-02 18:11:45 +0200

    eslint, prettier, and editorconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
     env: {
-        browser: true,
-        commonjs: true,
-        es2021: true,
-        node: true
+      browser: true,
+      es2021: true
     },
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
     parserOptions: {
-        ecmaVersion: 12
+      ecmaVersion: 12
     },
-    extends: ["plugin:prettier/recommended", "eslint:recommended"],
-    plugins: ["prettier"]
-};
+    rules: {
+    },
+    plugins: [
+      'prettier'
+    ]
+  };
