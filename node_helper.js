5dfd4e0aab24ea68869a2ae3dafa50e75aec4251
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-01-02 17:31:27 +0100

    Review ESLint process - Update ESLint dependencies - Fix lint scripts - Fix linting issues

--- node_helper.js
+++ node_helper.js
@@ -6,7 +6,7 @@
  */
 
 const NodeHelper = require('node_helper');
-const Log = require("logger");
+const Log = require('logger');
 const fetch = require('node-fetch');
 
 module.exports = NodeHelper.create({
@@ -38,9 +38,9 @@ module.exports = NodeHelper.create({
   clearTimeouts: function () {
     Log.debug(this.name, 'clearTimeouts');
     [...this.timeoutStandings, ...this.timeoutScorers, ...this.timeoutTable].forEach((id) => clearTimeout(id));
-    this.timeoutStandings.length = 0
-    this.timeoutScorers.length = 0
-    this.timeoutTable.length = 0
+    this.timeoutStandings.length = 0;
+    this.timeoutScorers.length = 0;
+    this.timeoutTable.length = 0;
   },
 
   start: function () {
@@ -49,7 +49,7 @@ module.exports = NodeHelper.create({
 
   stop: function () {
     Log.log('Stopping node helper for:', this.name);
-    this.clearTimeouts()
+    this.clearTimeouts();
   },
 
   doPost: async function (url, options) {
@@ -58,24 +58,24 @@ module.exports = NodeHelper.create({
     const localOptions = {
       ...this.requestOptions,
       ...options,
-      body: JSON.stringify({ lng: this.language })
-    }
+      body: JSON.stringify({ lng: this.language }),
+    };
     Log.debug(this.name, 'doPost', localUrl, localOptions);
-    const resp = await fetch(url, localOptions)
+    const resp = await fetch(url, localOptions);
     if (resp.status === 200) {
       data = await resp.json();
     } else {
       Log.error(this.name, 'doPost', localUrl, localOptions, resp);
-      data = null
+      data = null;
     }
-    return data
+    return data;
   },
 
   getLeagueIds: async function (leagues) {
-    this.clearTimeouts()
+    this.clearTimeouts();
     const url = `${this.baseURL}/competitions`;
     Log.info(this.name, 'getLeagueIds', url, leagues.join(', '));
-    const data = await this.doPost(url)
+    const data = await this.doPost(url);
     this.leaguesList = {};
     if (data) {
       if ('competitions' in data) {
@@ -94,14 +94,14 @@ module.exports = NodeHelper.create({
         });
       }
     }
-    Log.debug(this.name, 'getLeagueIds', this.leaguesList)
+    Log.debug(this.name, 'getLeagueIds', this.leaguesList);
     this.sendSocketNotification(this.name + '-LEAGUES', { leaguesList: this.leaguesList });
   },
 
   getTable: async function (leagueId) {
     const url = `${this.baseURL}/competitions/${leagueId.toString()}/table`;
     Log.debug(this.name, 'getTable', url);
-    const data = await this.doPost(url)
+    const data = await this.doPost(url);
     if (data) {
       Log.debug(this.name, 'getTable     | data', JSON.stringify(data, null, 2));
       if (!this.showStandings) {
@@ -113,8 +113,11 @@ module.exports = NodeHelper.create({
         table: tables,
       });
 
-      const nextRequest = new Date(new Date().getTime() + this.refreshTimeout[leagueId])
-      Log.info(this.name, `getTable     | next request for league "${this.leaguesList[leagueId].name} (${leagueId})" on ${nextRequest}`)
+      const nextRequest = new Date(new Date().getTime() + this.refreshTimeout[leagueId]);
+      Log.info(
+        this.name,
+        `getTable     | next request for league "${this.leaguesList[leagueId].name} (${leagueId})" on ${nextRequest}`
+      );
 
       this.timeoutTable[leagueId] = setTimeout(() => {
         this.getTable(leagueId);
@@ -126,35 +129,38 @@ module.exports = NodeHelper.create({
     const url = `${this.baseURL}/competitions/${leagueId.toString()}/matches/round/${round}`;
     Log.debug(this.name, 'getStandings', url);
 
-    const data = await this.doPost(url)
+    const data = await this.doPost(url);
     if (data) {
       const standings = data;
 
-      const allMatches = standings.data.filter(s => s.type === 'matches')
-      const allTimes = [...new Set(allMatches.map(m => m.time))].sort()
+      const allMatches = standings.data.filter((s) => s.type === 'matches');
+      const allTimes = [...new Set(allMatches.map((m) => m.time))].sort();
 
-      const tmp = {}
-      allTimes.forEach(t => {
-        const matchesAtSameTime = allMatches.filter(m => m.time === t)
-        const matchesTmp = [].concat.apply([], matchesAtSameTime.map(m => m.matches));
-        const toPlayMatches = matchesTmp.filter(m => ![60, 70, 90, 100, 110, 120].includes(m.status))
+      const tmp = {};
+      allTimes.forEach((t) => {
+        const matchesAtSameTime = allMatches.filter((m) => m.time === t);
+        const matchesTmp = [].concat.apply(
+          [],
+          matchesAtSameTime.map((m) => m.matches)
+        );
+        const toPlayMatches = matchesTmp.filter((m) => ![60, 70, 90, 100, 110, 120].includes(m.status));
         if (Array.isArray(toPlayMatches) && toPlayMatches.length > 0) {
-          tmp[t] = toPlayMatches
+          tmp[t] = toPlayMatches;
         }
-      })
+      });
 
-      let times = [...new Set(Object.keys(tmp).map(t => parseInt(t)))].sort()
+      let times = [...new Set(Object.keys(tmp).map((t) => parseInt(t)))].sort();
 
       Log.debug(this.name, 'getStandings | data', JSON.stringify(data, null, 2));
       this.refreshTime = (standings.refresh_time || 5 * 60) * 1000;
       Log.debug(this.name, 'getStandings | refresh_time', data.refresh_time, this.refreshTime);
 
-      const fiveMinutes = 60 * 5
+      const fiveMinutes = 60 * 5;
       const hundredTwentyMinutes = fiveMinutes * 24;
       const current_round = standings.current_round;
-      const rounds_detailed = data.rounds_detailed[current_round - 1]
-      const now = new Date().getTime() / 1000
-      let nextRequest = null
+      const rounds_detailed = data.rounds_detailed[current_round - 1];
+      const now = new Date().getTime() / 1000;
+      let nextRequest = null;
       this.refreshTimeout[leagueId] = this.refreshTime;
 
       const nextRoundRequest = () => {
@@ -163,33 +169,38 @@ module.exports = NodeHelper.create({
         let next_start = now + 24 * 12 * fiveMinutes; // now + 24 hours in minutes
         let deltaNowNextRequest = next_start * 1000;
         if (next_round <= selectable_rounds) {
-          const scheduleStart = data.rounds_detailed[current_round] && data.rounds_detailed[current_round].schedule_start || 0
+          const scheduleStart =
+            (data.rounds_detailed[current_round] && data.rounds_detailed[current_round].schedule_start) || 0;
           if (scheduleStart !== 0) {
-            next_start = scheduleStart - fiveMinutes
-            deltaNowNextRequest = next_start * 1000
+            next_start = scheduleStart - fiveMinutes;
+            deltaNowNextRequest = next_start * 1000;
           }
         } else {
-          this.refreshTimeout[leagueId] = null
+          this.refreshTimeout[leagueId] = null;
           clearInterval(this.timeoutStandings[leagueId]);
           clearInterval(this.timeoutTable[leagueId]);
           clearInterval(this.timeoutScorers[leagueId]);
         }
         this.refreshTimeout[leagueId] = deltaNowNextRequest;
-      }
+      };
 
       if (!rounds_detailed.schedule_start && !rounds_detailed.schedule_end) {
         this.refreshTimeout[leagueId] = 24 * 12 * fiveMinutes; // one day
-        nextRequest = new Date((now * 1000 + this.refreshTimeout[leagueId]));
+        nextRequest = new Date(now * 1000 + this.refreshTimeout[leagueId]);
       } else if (times.length >= 1) {
-        let start = times.length === 1 ? times[0] : times.reduce((prev, curr) => (Math.abs(curr - now) < Math.abs(prev - now) ? curr : prev)) // closest time to now
-        let startIndex = times.findIndex(t => t === start)
-        const end = startIndex === times.length - 1 ? start + hundredTwentyMinutes : times[times.length - 1] += fiveMinutes
+        let start =
+          times.length === 1
+            ? times[0]
+            : times.reduce((prev, curr) => (Math.abs(curr - now) < Math.abs(prev - now) ? curr : prev)); // closest time to now
+        let startIndex = times.findIndex((t) => t === start);
+        const end =
+          startIndex === times.length - 1 ? start + hundredTwentyMinutes : (times[times.length - 1] += fiveMinutes);
 
-        start -= fiveMinutes
+        start -= fiveMinutes;
 
         // now is in between the start and the end time of the event
         if (now >= start && end > 0 && now <= end) {
-          nextRequest = new Date((now * 1000 + this.refreshTime));
+          nextRequest = new Date(now * 1000 + this.refreshTime);
           // now is before the start of the event
         } else if (now < start) {
           const deltaNowStart = start - now;
@@ -206,20 +217,21 @@ module.exports = NodeHelper.create({
 
       // https://developer.mozilla.org/en-US/docs/Web/API/setTimeout#:~:text=Maximum%20delay%20value,the%20timeout%20being%20executed%20immediately.
       // https://stackoverflow.com/a/56718027/448660
-      const MAX_TIMEOUT_VALUE = 2147483647
+      const MAX_TIMEOUT_VALUE = 2147483647;
 
       if (this.refreshTimeout[leagueId] > MAX_TIMEOUT_VALUE) {
-        this.refreshTimeout[leagueId] = MAX_TIMEOUT_VALUE
-        nextRequest = new Date(this.refreshTimeout[leagueId] + new Date().getTime())
+        this.refreshTimeout[leagueId] = MAX_TIMEOUT_VALUE;
+        nextRequest = new Date(this.refreshTimeout[leagueId] + new Date().getTime());
       }
 
       this.timeoutStandings[leagueId] = setTimeout(() => {
         this.getStandings(leagueId, round);
       }, this.refreshTimeout[leagueId]);
 
-      const round_title = rounds_detailed.round_title
-      Log.info(this.name, `getStandings | next request for league "${this.leaguesList[leagueId].name} (${leagueId})" on ${nextRequest}`)
-
+      Log.info(
+        this.name,
+        `getStandings | next request for league "${this.leaguesList[leagueId].name} (${leagueId})" on ${nextRequest}`
+      );
 
       const doRequest = () => {
         const forLoop = async () => {
@@ -229,37 +241,39 @@ module.exports = NodeHelper.create({
                 const matches = s.matches;
                 for (let m of matches) {
                   const d = await this.getDetails(leagueId, m.match_id);
-                  const details = d && d.filter(t => t.type === 'details');
+                  const details = d && d.filter((t) => t.type === 'details');
                   Log.debug(this.name, 'getStandings | details', m.match_id, JSON.stringify(details, null, 2));
-                  m.details = details && details[0] ? details[0].details : []
-                  let match_info = d && d.filter(t => t.type === 'match_info');
-                  match_info = match_info && match_info[0] ? match_info[0].match_info : []
-                  match_info = match_info.info_items.filter(m => !['stream', 'promotion'].includes(m.info_type))
+                  m.details = details && details[0] ? details[0].details : [];
+                  let match_info = d && d.filter((t) => t.type === 'match_info');
+                  match_info = match_info && match_info[0] ? match_info[0].match_info : [];
+                  match_info = match_info.info_items.filter((m) => !['stream', 'promotion'].includes(m.info_type));
                   Log.debug(this.name, 'getStandings | match_info', m.match_id, JSON.stringify(match_info, null, 2));
-                  m.match_info = match_info
+                  m.match_info = match_info;
                 }
               }
             }
           }
-        }
+        };
 
         forLoop().then(() => {
-          this.refreshTime = this.refreshTimeout[leagueId]
+          this.refreshTime = this.refreshTimeout[leagueId];
           this.sendSocketNotification(this.name + '-STANDINGS', {
             leagueId: leagueId,
             standings: standings,
-            nextRequest: nextRequest
+            nextRequest: nextRequest,
           });
-        })
-      }
+        });
+      };
 
       doRequest();
-
     } else {
       Log.error(this.name, 'getStandings', data);
-      this.timeoutStandings[leagueId] = setTimeout(() => {
-        this.getStandings(leagueId, round);
-      }, 5 * 60 * 1000);
+      this.timeoutStandings[leagueId] = setTimeout(
+        () => {
+          this.getStandings(leagueId, round);
+        },
+        5 * 60 * 1000
+      );
     }
   },
 
@@ -267,14 +281,19 @@ module.exports = NodeHelper.create({
     const url = `${this.baseURL}/competitions/${leagueId.toString()}/scorers`;
     Log.debug(this.name, 'getScorers', url);
 
-    const data = await this.doPost(url)
+    const data = await this.doPost(url);
     if (data) {
       Log.debug(this.name, 'getScorers   | data', JSON.stringify(data, null, 2));
       if (!this.showStandings) {
         this.refreshTime = (data.refresh_time || 5 * 60) * 1000;
       }
-      Log.debug(this.name, 'getScorers   | refresh_time', data.refresh_time, (this.refreshTimeout[leagueId] || this.refreshTime));
-      const scorers = data.data.filter(d => d.type === 'scorers' && d.scorers) || [];
+      Log.debug(
+        this.name,
+        'getScorers   | refresh_time',
+        data.refresh_time,
+        this.refreshTimeout[leagueId] || this.refreshTime
+      );
+      const scorers = data.data.filter((d) => d.type === 'scorers' && d.scorers) || [];
       this.sendSocketNotification(this.name + '-SCORERS', {
         leagueId: leagueId,
         scorers: scorers,
@@ -283,13 +302,19 @@ module.exports = NodeHelper.create({
         this.getScorers(leagueId);
       }, this.refreshTimeout[leagueId] || this.refreshTime);
 
-      const nextRequest = new Date(new Date().getTime() + (this.refreshTimeout[leagueId] || this.refreshTime))
-      Log.info(this.name, `getScorers   | next request for league "${this.leaguesList[leagueId].name} (${leagueId})" on ${nextRequest}`)
+      const nextRequest = new Date(new Date().getTime() + (this.refreshTimeout[leagueId] || this.refreshTime));
+      Log.info(
+        this.name,
+        `getScorers   | next request for league "${this.leaguesList[leagueId].name} (${leagueId})" on ${nextRequest}`
+      );
     } else {
       Log.error(this.name, 'getScorers', data);
-      this.timeoutScorers[leagueId] = setTimeout(() => {
-        this.getScorers(leagueId);
-      }, this.refreshTimeout[leagueId] || 5 * 60 * 1000);
+      this.timeoutScorers[leagueId] = setTimeout(
+        () => {
+          this.getScorers(leagueId);
+        },
+        this.refreshTimeout[leagueId] || 5 * 60 * 1000
+      );
     }
   },
 
@@ -297,30 +322,29 @@ module.exports = NodeHelper.create({
     const url = `${this.baseURL}/competitions/${leagueId.toString()}/matches/${matchId.toString()}/details`;
     Log.debug(this.name, 'getDetails', leagueId, url);
 
-    let details = []
-    return new Promise(async (resolve, _reject) => {
-      const data = await this.doPost(url)
-      if (data && data.data) {
-        Log.debug(this.name, 'getDetails   | data', leagueId, JSON.stringify(data, null, 2));
-        Log.debug(this.name, 'getDetails   | data', 'leagueId', leagueId, 'matchId', matchId);
-        details = data.data || [];
-        resolve(details);
-      } else {
-        resolve([]);
-        Log.error(this.name, 'getDetails', leagueId, url, data);
-      }
-    });
+    let details = await this.doPost(url);
+
+    if (details && details.data) {
+      Log.debug(this.name, 'getDetails   | data', leagueId, JSON.stringify(details, null, 2));
+      Log.debug(this.name, 'getDetails   | data', 'leagueId', leagueId, 'matchId', matchId);
+      details = details.data || [];
+    } else {
+      details = [];
+      Log.error(this.name, 'getDetails', leagueId, url, details);
+    }
+
+    return details;
   },
 
   socketNotificationReceived: function (notification, payload) {
-    Log.debug(this.name, 'socketNotificationReceived', notification, payload)
+    Log.debug(this.name, 'socketNotificationReceived', notification, payload);
     if (notification === this.name + '-CONFIG') {
       this.showStandings = payload.showStandings;
       this.showDetails = this.showStandings && payload.showDetails;
       this.showTables = payload.showTables;
       this.showScorers = payload.showScorers;
       if (payload.language) {
-        this.language = this.supportedLanguages.includes(payload.language) ? payload.language : 'en'
+        this.language = this.supportedLanguages.includes(payload.language) ? payload.language : 'en';
       }
       this.getLeagueIds(payload.leagues);
     }
